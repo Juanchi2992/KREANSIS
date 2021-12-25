a76f1ea718adce551fec360b3ab758a2f046a6c3
Juan <juanchipicabohm2992@gmail.com>
2021-12-25 20:53:10 -0300

    KREANSIS

---
+++ scroll.js
@@ -0,0 +1,40 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.carousel',{
+        duration: 3000
+    });
+
+    // sr.reveal('.navbar-nav',{
+    //     duration: 3000,
+    //     origin: 'left',
+    //     distance: '-100px'
+    // });
+
+    sr.reveal('.navbar-brand',{
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.p-nosotros',{
+        duration: 3000
+    });
+
+    sr.reveal('.elegirnos',{
+        duration: 3000
+    });
+
+    sr.reveal('.d1-3',{
+        duration: 3000
+    });
+
+    sr.reveal('.redessociales',{
+        duration: 3000,
+        origin: 'bottom',
+        distance: '100px'
+
+    });
+
+    sr.reveal('.float',{
+        duration: 3000
+    });
